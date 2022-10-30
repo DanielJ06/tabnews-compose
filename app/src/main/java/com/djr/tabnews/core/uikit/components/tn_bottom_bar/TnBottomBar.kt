6e36f27ff38f23c0685fd0406d4cc3ebf0bdc3fb
danieljesus <daniel.j.rodrigues06@gmail.com>
2022-10-30 11:33:17 -0300

    ⚙️️ refactor: refactoring BottomNavigationItem to be isolated, and let the caller handle logics

--- app/src/main/java/com/djr/tabnews/core/uikit/components/tn_bottom_bar/TnBottomBar.kt
+++ app/src/main/java/com/djr/tabnews/core/uikit/components/tn_bottom_bar/TnBottomBar.kt
@@ -4,18 +4,12 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.CornerRadius
@@ -73,50 +67,38 @@ fun TnBottomBar(navController: NavController) {
                     TabNewsTheme.colors.textNeutral
                 }
 
-                var selectedPosition = 0f
-
-                if (isSelected) {
-                    coroutineScope.launch {
-                        iconCoordinates.animateTo(
-                            selectedPosition,
-                            animationSpec = tween(
-                                durationMillis = 150,
-                                easing = CubicBezierEasing(.57f, .21f, .69f, 1.25f)
-                            )
-                        )
+                fun itemClick() {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
                     }
                 }
 
-                BottomNavigationItem(
-                    selected = isSelected,
-                    icon = {
-                        Icon(
-                            imageVector = screen.icon,
-                            contentDescription = null,
-                            tint = iconTint,
-                            modifier = Modifier
-                                .size(TabNewsTheme.spacing.Xxs)
-                                .onGloballyPositioned {
-                                    val axisX = it.positionInRoot().x
-                                    val icHalfSize = it.size.width / 2
-                                    val icCenter = axisX + icHalfSize
-                                    coroutineScope.launch {
-                                        if (
-                                            iconCoordinates.value == 0f &&
-                                            topDestinationScreens[0] == screen
-                                        ) iconCoordinates.snapTo(icCenter)
-                                    }
-                                    selectedPosition = icCenter
-                                }
-                        )
+                BottomNavItem(
+                    isSelected = isSelected,
+                    screen = screen,
+                    iconTint = iconTint,
+                    onClick = { itemClick() },
+                    onPositioned = { axis ->
+                        coroutineScope.launch {
+                            if (
+                                iconCoordinates.value == 0f &&
+                                topDestinationScreens[0] == screen
+                            ) iconCoordinates.snapTo(axis)
+                        }
                     },
-                    onClick = {
-                        navController.navigate(screen.route) {
-                            popUpTo(navController.graph.findStartDestination().id) {
-                                saveState = true
-                            }
-                            launchSingleTop = true
-                            restoreState = true
+                    onSelected = { axis ->
+                        coroutineScope.launch {
+                            iconCoordinates.animateTo(
+                                axis,
+                                animationSpec = tween(
+                                    durationMillis = 150,
+                                    easing = CubicBezierEasing(.57f, .21f, .69f, 1.25f)
+                                )
+                            )
                         }
                     }
                 )
@@ -148,4 +130,41 @@ private fun FloatingIndicatorPill(
             )
         )
     }
+}
+
+@Composable
+fun RowScope.BottomNavItem(
+    isSelected: Boolean,
+    screen: TnBottomScreens,
+    iconTint: Color,
+    onClick: () -> Unit,
+    onPositioned: (icPosition: Float) -> Unit,
+    onSelected: (icPosition: Float) -> Unit
+) {
+    var selectedAxisX by remember { mutableStateOf(0f) }
+
+    if (isSelected) {
+        onSelected(selectedAxisX)
+    }
+
+    BottomNavigationItem(
+        selected = isSelected,
+        onClick = onClick,
+        icon = {
+            Icon(
+                imageVector = screen.icon,
+                contentDescription = null,
+                tint = iconTint,
+                modifier = Modifier
+                    .size(TabNewsTheme.spacing.Xxs)
+                    .onGloballyPositioned {
+                        val axisX = it.positionInRoot().x
+                        val icHalfSize = it.size.width / 2
+                        val icCenter = axisX + icHalfSize
+                        selectedAxisX = icCenter
+                        onPositioned(icCenter)
+                    }
+            )
+        }
+    )
 }
