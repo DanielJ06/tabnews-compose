78d361ad25804ef61007712bb6816eb86015d1d9
danieljesus <daniel.j.rodrigues06@gmail.com>
2022-11-12 13:35:22 -0300

    🗂️ dependencies: moving to version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "dagger.hilt.android.plugin" -> {
+                    useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
+                }
+            }
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tabnews"
+include(":app")
+include(":core")
+include(":core:data")
+
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
