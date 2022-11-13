611878fea861fbc3b3688f0e55cc5cbb3ab30e6b
danieljesus <daniel.j.rodrigues06@gmail.com>
2022-11-13 09:39:24 -0300

    🔁 gradle: change from groovy to DSL

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,28 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.lib)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.djr.tabnews.core.data"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core)
+}
